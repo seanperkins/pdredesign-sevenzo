3d9e059348ccc5b94a9bdbd688c6979ce1493b30
Sumeet Singh <ortuna@gmail.com>
2014-04-21 18:30:39 -0400

    Fixed specs for isolated pdr_client engine

--- spec/routing/pdr_client_spec.rb
+++ spec/routing/pdr_client_spec.rb
@@ -5,7 +5,7 @@ describe 'PdrClient Engine' do
 
   it 'routes root to the angular engine' do
     expect(get: "/").to route_to(
-      controller: "pdr_client",
+      controller: "pdr_client/pdr_client",
       action: 'index'
     )
   end
