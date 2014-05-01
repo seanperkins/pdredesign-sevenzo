0c5560f6a07398d18a78646049260ff87835e025
Sumeet Singh <ortuna@gmail.com>
2014-05-01 13:51:36 -0400

    Better fallback url

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,3 @@
-# encoding: utf-8
-
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
@@ -13,7 +11,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    "/fallback/" + [version_name, "default.png"].compact.join('_')
+    ActionController::Base
+      .helpers
+      .asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   end
 
   # Process files as they are uploaded:
