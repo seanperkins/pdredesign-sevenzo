dca9575d6635c573bf5113c31fb39e400e4e25a5
Sumeet Singh <ortuna@gmail.com>
2014-05-14 11:05:59 -0400

    Removed domain :all for session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-PdrServer::Application.config.session_store :active_record_store, :key => '_pdr_server_session', :domain => :all
+PdrServer::Application.config.session_store :active_record_store, :key => '_pdr_server_session'
