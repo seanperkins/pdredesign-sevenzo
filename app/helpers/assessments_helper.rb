cdc666636ae691bbb70c2011a192817a4c910517
Sumeet Singh <ortuna@gmail.com>
2014-06-30 10:48:48 -0400

    give a default message for assessments

---
+++ app/helpers/assessments_helper.rb
@@ -0,0 +1,5 @@
+module AssessmentsHelper
+  def default_assessment_message
+    "The goal of this assessment is to discuss professional development and determine areas of strength or limitations within your district. To participate along with your colleagues, you must first register with the PD Readiness Assessment Tool. Once registered, you’ll be able to answer the questions and have on-going access to the assessment discussions."
+  end
+end
