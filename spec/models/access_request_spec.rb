2c571db4575bdc3a77275a0ae3fa6fa891b4813d
Sumeet Singh <ortuna@gmail.com>
2014-07-16 14:36:07 -0400

    Updated annotation and create default toolkit task to use subcategory

--- spec/models/access_request_spec.rb
+++ spec/models/access_request_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: access_requests
+#
+#  id            :integer          not null, primary key
+#  assessment_id :integer
+#  user_id       :integer
+#  roles         :string(255)      default([]), is an Array
+#  created_at    :datetime
+#  updated_at    :datetime
+#  token         :string(255)
+#
+
 require 'spec_helper'
 
 describe AccessRequest do
