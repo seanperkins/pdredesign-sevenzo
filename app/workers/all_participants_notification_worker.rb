129d465c4305cea46dcd3d46e574b0599b326351
Sumeet Singh <ortuna@gmail.com>
2014-08-13 19:09:43 -0400

    Fixed passing participant object instead of email

--- app/workers/all_participants_notification_worker.rb
+++ app/workers/all_participants_notification_worker.rb
@@ -3,10 +3,10 @@ class AllParticipantsNotificationWorker
 
   def perform(assessment_id)
     assessment = Assessment.find(assessment_id)
-
     assessment.participants.each do |participant|
+
       AssessmentsMailer
-        .assigned(assessment, participant.user.email)
+        .assigned(assessment, participant)
         .deliver
     end
   end
