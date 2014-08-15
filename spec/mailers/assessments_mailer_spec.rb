f8c08a25ccca6ab0749cd1aec4f7b375d22732d9
Sumeet Singh <ortuna@gmail.com>
2014-08-15 09:30:37 -0400

    Assesment specs

--- spec/mailers/assessments_mailer_spec.rb
+++ spec/mailers/assessments_mailer_spec.rb
@@ -2,8 +2,21 @@ require 'spec_helper'
 
 describe AssessmentsMailer do
   before { create_magic_assessments }
+  let(:assessment) { @assessment_with_participants }
+  let(:subject)    { AssessmentsMailer }
+
 
   describe '#assigned' do
-    it 'it sends the email to the correct email'
+    it 'sends the email to the correct email' do
+      mail = AssessmentsMailer.assigned(assessment, @participant)
+      expect(mail.to).to include(@participant.user.email)
+    end
+  end
+
+  describe '#reminder' do
+    it 'sends the meail to the correct participant' do  
+      mail = AssessmentsMailer.reminder(assessment, 'reminder', @participant)
+      expect(mail.to).to include(@participant.user.email)
+    end
   end
 end
