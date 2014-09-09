72ae23497cb0f329e05d9d0b08a1553b6cb52ca4
Sumeet Singh <ortuna@gmail.com>
2014-09-09 10:56:14 -0400

    Less fragil migration

--- db/migrate/20140908154941_change_toolkit_financial_budget.rb
+++ db/migrate/20140908154941_change_toolkit_financial_budget.rb
@@ -1,8 +1,9 @@
 class ChangeToolkitFinancialBudget < ActiveRecord::Migration
   def up
     tk = Tool.find_by(title: 'Financial Systems & Budget')
-    tk.update(title: "Financial Systems & Budget Allocation Diagnostic")
-
-    Tool.find_by(title: 'Allocation Diagnostic').destroy
+    if tk
+      tk.update(title: "Financial Systems & Budget Allocation Diagnostic")
+      Tool.find_by(title: 'Allocation Diagnostic').destroy
+    end
   end
 end
