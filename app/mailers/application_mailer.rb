c34eaee116ef81e52f81325b3f5448238d53d439
Sumeet Singh <ortuna@gmail.com>
2014-10-16 13:13:07 -0400

    @pdredesign.org => @mail.pdredesign.org

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'support@pdredesign.org'
+  default from: 'support@mail.pdredesign.org'
   default from_name: 'PD Redesign'
   layout 'application_mailer'
 end
