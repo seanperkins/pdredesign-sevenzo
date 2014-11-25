e99fca1b313617a7fd39efdde3fa81e4767a7ac4
Sumeet Singh <ortuna@gmail.com>
2014-11-25 16:18:28 -0500

    Masquerade feature for rails_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,8 @@
 
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :masqueradable
 
   include Authority::UserAbilities
   has_many :assessments, through: :participants
