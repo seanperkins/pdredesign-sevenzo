4a663009d32eb4a8ce2ee19cb964d3355546020e
bithavoc <im@bithavoc.io>
2016-02-18 14:20:55 -0500

    Refactor Shared Report controller to reuse Original Report controller

--- app/controllers/v1/shared_report_controller.rb
+++ app/controllers/v1/shared_report_controller.rb
@@ -1,51 +1,12 @@
-class V1::SharedReportController < ApplicationController
+class V1::SharedReportController < V1::ReportController
+  skip_before_action :authenticate_user!
+  skip_before_action :authorize_assessment!
+
   before_filter :load_assessment!
   delegate :axis_questions, to: :report
   delegate :average, to: :report
-
-  def show
-    @assessment = assessment
-    @response   = @assessment.response
-    @axes       = report.axes
-    update_participant
-    render 'v1/report/show'
-  end
-
-  def consensus_report
-    @assessment   = assessment
-    @rubric       = assessment.rubric
-    @response     = consensus
-    if @response
-      @categories   = @response.categories  
-    else
-      not_found
-    end
-  end
-
-  def participant_consensu_report
-    @assessment   = assessment
-    @consensus    = consensus
-    if @assessment && @consensus
-      @participant  = Participant.find_by(assessment: assessment)
-    else
-      not_found
-    end
-  end
-
+  
   private
-  def report 
-    @report ||= Assessments::Report.new(assessment)
-  end
-
-  def update_participant
-    return unless participant
-    participant.update(report_viewed_at: Time.now)
-  end
-
-  def participant
-    Participant.find_by(assessment: assessment, user: current_user)
-  end
-
   def assessment
     @assessment ||= Assessment.where(share_token: request_share_token).first
   end
@@ -57,9 +18,5 @@ class V1::SharedReportController < ApplicationController
 
   def request_share_token
     params[:shared_id]
-  end
-
-  def consensus
-    Response.find_by(responder_type: 'Assessment', id: params[:consensu_id])
   end
 end
