f636960da7c56692456d7cd40b6a256099b6d8ff
Jason Black <makotothedragon@gmail.com>
2016-02-19 10:56:49 -0700

    Learning Question: Creation

--- app/assets/javascripts/client/directives/learning_questions/learningQuestionDisplay.js
+++ app/assets/javascripts/client/directives/learning_questions/learningQuestionDisplay.js
@@ -1,5 +1,6 @@
 (function () {
-  PDRClient.directive('learningQuestionDisplay', learningQuestionDisplay);
+  angular.module('PDRClient')
+      .directive('learningQuestionDisplay', learningQuestionDisplay);
 
   function learningQuestionDisplay() {
     return {
@@ -7,25 +8,28 @@
       templateUrl: 'client/views/directives/learning_questions/display.html',
       replace: true,
       transclude: true,
-      controller: ['$stateParams', 'LearningQuestion', LearningQuestionDisplayCtrl],
+      controller: ['$stateParams', '$scope', 'LearningQuestion', LearningQuestionDisplayCtrl],
       controllerAs: 'learningQuestionDisplay'
     }
   }
 
-  function LearningQuestionDisplayCtrl($stateParams, LearningQuestion) {
+  function LearningQuestionDisplayCtrl($stateParams, $scope, LearningQuestion) {
     var vm = this;
     vm.learningQuestions = [];
-    vm.editorOpened = false;
 
-    LearningQuestion
-        .get({assessment_id: $stateParams.id})
-        .$promise
-        .then(function (result) {
-          vm.learningQuestions = result.learning_questions;
-        });
+    vm.loadQuestions = function () {
+      LearningQuestion
+          .get({assessment_id: $stateParams.id})
+          .$promise
+          .then(function (result) {
+            vm.learningQuestions = result.learning_questions;
+          });
+    };
 
-    vm.toggleEditor = function() {
-      vm.editorOpened = !vm.editorOpened;
-    }
+    $scope.$on('new-learning-question', function () {
+      vm.loadQuestions();
+    });
+
+    vm.loadQuestions();
   }
 })();
--- app/assets/javascripts/client/directives/learning_questions/learningQuestionForm.js
+++ app/assets/javascripts/client/directives/learning_questions/learningQuestionForm.js
@@ -1,25 +1,38 @@
 (function () {
-  PDRClient.directive('learningQuestionForm', learningQuestionForm);
+  angular.module('PDRClient')
+      .directive('learningQuestionForm', learningQuestionForm);
 
   function learningQuestionForm() {
     return {
       restrict: 'E',
       templateUrl: 'client/views/directives/learning_questions/form.html',
       replace: true,
-      controller: LearningQuestionFormCtrl,
+      controller: ['$stateParams', '$scope', 'LearningQuestion', LearningQuestionFormCtrl],
       controllerAs: 'learningQuestionForm',
       transclude: true
     }
   }
 
-  function LearningQuestionFormCtrl() {
+  function LearningQuestionFormCtrl($stateParams, $scope, LearningQuestion) {
     var vm = this;
-    vm.model = {
+    vm.newEntity = {
       'body': ''
+    };
+    vm.status = '';
+
+    vm.clearInputForm = function(model) {
+      model.body = '';
     };
 
     vm.createLearningQuestion = function(model) {
-      console.log(model);
+      LearningQuestion
+          .create({assessment_id: $stateParams.id}, {learning_question: {body: model.body}})
+          .$promise
+          .then(function(result) {
+            vm.status = result;
+            $scope.$emit('new-learning-question');
+            vm.clearInputForm(model);
+          });
     }
   }
 })();
