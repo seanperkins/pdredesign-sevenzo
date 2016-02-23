f6cff4d844cca1658465808321d47b0f07f36596
bithavoc <im@bithavoc.io>
2016-02-22 20:42:38 -0500

    Enhance Spec for AssessmentSharedReportCtrl

--- app/assets/javascripts/client/controllers/assessments/AssessmentSharedReportCtrl.js
+++ app/assets/javascripts/client/controllers/assessments/AssessmentSharedReportCtrl.js
@@ -1,19 +1,12 @@
 PDRClient.controller('AssessmentSharedReportCtrl', [
   '$scope', 
-  '$http',
-  '$timeout', 
-  '$anchorScroll',
   '$stateParams',
   'SharedAssessment', 
   'SharedReport',
-  function($scope, $http, $timeout, $anchorScroll, $stateParams, SharedAssessment, SharedReport) {
+  function($scope, $stateParams, SharedAssessment, SharedReport) {
       var token = $stateParams.token;
       $scope.token = token;
       $scope.assessment = SharedAssessment.get({token: token});
       $scope.report = SharedReport.get({token: token});
-
-      $scope.isFacilitator = function() {
-        return false;
-      };
     }
 ]);
