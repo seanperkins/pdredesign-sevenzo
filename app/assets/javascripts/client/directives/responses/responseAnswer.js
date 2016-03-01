7cd27bd0b470ddd5d02f4fefdb09f92af0c3cee2
Jason Black <makotothedragon@gmail.com>
2016-03-01 10:34:55 -0700

    Refactor answer to individual directive

---
+++ app/assets/javascripts/client/directives/responses/responseAnswer.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+  angular.module('PDRClient')
+      .directive('responseAnswer', responseAnswer);
+
+  function responseAnswer() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {
+        question: '=',
+        answer: '='
+      },
+      templateUrl: 'client/views/directives/responses/answer.html',
+      controller: 'ResponseAnswerCtrl',
+      controllerAs: 'responseAnswer'
+    }
+  }
+})();
