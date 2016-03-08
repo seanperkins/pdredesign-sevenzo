1f9d286a29255e2cd46fce66a1e6afda89347914
Jason Black <makotothedragon@gmail.com>
2016-03-08 15:37:46 -0700

    Ensure Data and Tech Audit tab is properly colored in nav

--- app/assets/javascripts/client/controllers/NavigationCtrl.js
+++ app/assets/javascripts/client/controllers/NavigationCtrl.js
@@ -1,47 +1,62 @@
-PDRClient.controller('NavigationCtrl', ['$scope', '$rootScope', 'SessionService', '$location', '$modal',
-    function($rootScope, $scope, SessionService, $location, $modal) {
-      $scope.updateTemplate = function() {
-        SessionService.setUserTemplate(
+(function() {
+  'use strict';
+
+  angular.module('PDRClient')
+      .controller('NavigationCtrl', NavigationCtrl);
+
+  NavigationCtrl.$inject = [
+    '$scope',
+    'SessionService',
+    '$location'
+  ];
+
+  function NavigationCtrl($scope, SessionService, $location) {
+    $scope.updateTemplate = function() {
+      SessionService.setUserTemplate(
           $scope,
           'client/views/navigation/navigation_user.html',
           'client/views/navigation/navigation_anon.html'
-        );
-      };
-      $scope.updateTemplate();
+      );
+    };
+    $scope.updateTemplate();
+    $scope.user = SessionService.getCurrentUser();
+
+    $scope.currentLocation = '';
+    $scope.activeClassFor = function(location) {
+      if ($scope.currentLocation === location) {
+        return 'active';
+      }
+      return '';
+    };
+
+    $scope.$watch(function() {
+      return $location.url();
+    }, function(url) {
+      switch (url) {
+        case '/assessments':
+          return $scope.currentLocation = "current_state";
+        case '':
+          return $scope.currentLocation = "home";
+        case '/':
+          return $scope.currentLocation = "home";
+        case '/login':
+          return $scope.currentLocation = "login";
+        case '/administrators':
+          return $scope.currentLocation = "administrators";
+        case '/educators':
+          return $scope.currentLocation = "educators";
+          return $scope.currentLocation = "networks";
+        case '/inventories':
+          return $scope.currentLocation = 'inventories';
+        default:
+          return '';
+      }
+    });
+
+    $scope.$on('session_updated', function() {
+      SessionService.syncUser();
       $scope.user = SessionService.getCurrentUser();
-
-      $scope.currentLocation = '';
-      $scope.activeClassFor = function(location) {
-        if($scope.currentLocation == location)
-          return 'active';
-        return '';
-      };
-      
-      $scope.$watch(function () { return $location.url(); }, function (url) {
-        switch(url) {
-          case '/assessments':
-            return $scope.currentLocation = "current_state";
-          case '':
-            return $scope.currentLocation = "home";
-          case '/':
-            return $scope.currentLocation = "home";
-          case '/login':
-            return $scope.currentLocation = "login";
-          case '/administrators':
-            return $scope.currentLocation = "administrators";
-          case '/educators':
-            return $scope.currentLocation = "educators";
-            case '/networks':
-            return $scope.currentLocation = "networks";
-          default:
-            return '';
-        }
-      });
-
-      $scope.$on('session_updated', function() {
-        SessionService.syncUser();
-        $scope.user = SessionService.getCurrentUser();
-        $scope.updateTemplate();
-      });
-    }
-]);
+      $scope.updateTemplate();
+    });
+  }
+})();
