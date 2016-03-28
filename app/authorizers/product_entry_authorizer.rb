26f19ef02956bcd39d909c98ab0aaa56cbfce14f
Júlio Santos <hi@whoisjuliosantos.com>
2016-03-28 12:55:46 +0100

    better naming in ProductEntryAuthorizer

--- app/authorizers/product_entry_authorizer.rb
+++ app/authorizers/product_entry_authorizer.rb
@@ -1,18 +1,18 @@
 class ProductEntryAuthorizer < ApplicationAuthorizer
   def readable_by?(user)
-    is_owner_or_belongs_to_district?(user)
+    is_owner_or_member_of_inventory?(user)
   end
 
   def creatable_by?(user)
-    is_owner_or_belongs_to_district?(user)
+    is_owner_or_member_of_inventory?(user)
   end
 
   def updatable_by?(user)
-    is_owner_or_belongs_to_district?(user)
+    is_owner_or_member_of_inventory?(user)
   end
 
   private
-  def is_owner_or_belongs_to_district?(user)
+  def is_owner_or_member_of_inventory?(user)
     resource.inventory.try(:owner?, user: user) ||
         resource.inventory.try(:participant?, user: user) ||
         resource.inventory.try(:facilitator?, user: user) ||
