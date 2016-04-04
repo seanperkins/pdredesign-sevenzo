f0c3655b2f349a2b8e2a180f9881522cd4bebff0
bithavoc <im@bithavoc.io>
2016-04-04 18:24:48 -0500

    Code-review fixes

--- app/assets/javascripts/client/inventories/InventoryInvitation.js
+++ app/assets/javascripts/client/inventories/InventoryInvitation.js
@@ -1,11 +1,13 @@
 (function() {
   'use strict';
+  angular.module('PDRClient').factory('InventoryInvitation', InventoryInvitation);
+  InventoryInvitation.$inject = ['$resource', 'UrlService'];
 
-  angular.module('PDRClient').factory('InventoryInvitation', ['$resource', 'UrlService', function($resource, UrlService) {
+  function InventoryInvitation($resource, UrlService) {
     return $resource(UrlService.url('inventories/:inventory_id/invitations'), null,{
       create: {
         method: 'POST',
       }
     });
-  }]);
+  }
 })();
--- app/assets/javascripts/client/inventories/InviteInventoryUserCtrl.js
+++ app/assets/javascripts/client/inventories/InviteInventoryUserCtrl.js
@@ -7,21 +7,20 @@
   function InviteInventoryUserCtrl($scope, InventoryInvitation) {
     var vm = this;
     vm.sendInvitation = function(invitation) {
-      $scope.alerts  = [];
-      $scope.addAlert = function(message) {
-        $scope.alerts.push({type: 'danger', msg: message});
+      vm.alerts  = [];
+      vm.addAlert = function(message) {
+        vm.alerts.push({type: 'danger', msg: message});
       };
 
-      $scope.closeAlert = function(index) {
-        $scope.alerts.splice(index, 1);
+      vm.closeAlert = function(index) {
+        vm.alerts.splice(index, 1);
       };
       InventoryInvitation.create({ inventory_id: $scope.inventoryId }, invitation).$promise.then(function(response) {
-        var errors = response.data.errors;
         $scope.$emit('invite-sent');
       }).catch(function(response) {
         var errors = response.data.errors;
         angular.forEach(errors, function(error, field) {
-          $scope.addAlert(field + " : " + error);
+          vm.addAlert(field + " : " + error);
         });
       });
     };
