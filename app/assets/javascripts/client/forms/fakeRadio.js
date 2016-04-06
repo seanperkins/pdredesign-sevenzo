926c583f1b40192eabedf61ade8b337674dda06f
Júlio Santos <hi@whoisjuliosantos.com>
2016-04-06 10:20:11 +0100

    incorporated Jason's review suggestions

--- app/assets/javascripts/client/forms/fakeRadio.js
+++ app/assets/javascripts/client/forms/fakeRadio.js
@@ -1,15 +1,17 @@
 (function() {
   'use strict';
   angular.module('PDRClient')
-      .directive('fakeRadio', function () {
-        return {
-          restrict: 'E',
-          scope: {
-            model: '=',
-            value: '@'
-          },
-          replace: true,
-          templateUrl: 'client/forms/fake_radio.html'
-        }
-      });
+      .directive('fakeRadio', fakeRadio);
+  
+  function fakeRadio () {
+    return {
+      restrict: 'E',
+      scope: {
+        model: '=',
+        value: '@'
+      },
+      replace: true,
+      templateUrl: 'client/forms/fake_radio.html'
+    }
+  };
 })();
