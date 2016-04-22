1534b63c7e99f66d40477d658ecb77ee0b539100
Júlio Santos <hi@whoisjuliosantos.com>
2016-04-22 19:13:32 +0100

    default analysis message

--- app/helpers/invite_tool_message_helper.rb
+++ app/helpers/invite_tool_message_helper.rb
@@ -8,4 +8,10 @@ module InviteToolMessageHelper
 
     To participate along with your colleagues, you must first register with the PDredesign. Once registered, you'll be able to fill out the inventory and have ongoing access to the inventory, assessment and results. Please contact #{current_user.name} with any questions at #{current_user.email}."
   end
+
+  def default_analysis_message
+    "The goal of the Data & Tech Analysis is to get the big picture of data and technology systems that support professional learning in your district. The Analysis consists of an in-person discussion of how well current data systems, technology infrastructure, and product purchases are aligned to professional development goals.
+
+    To participate along with your colleagues, you must first register with the PDredesign. Once registered, you'll have ongoing access to the analysis and results. Please contact #{current_user.name} with any questions at #{current_user.email}."
+  end
 end
