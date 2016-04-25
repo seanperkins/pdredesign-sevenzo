f55396bf30afa17c88a66d5f163c9387c0efac4f
bithavoc <im@bithavoc.io>
2016-04-25 10:04:49 -0500

    enhance download links

--- app/assets/javascripts/client/inventories/InventoryReportDownloadCtrl.js
+++ app/assets/javascripts/client/inventories/InventoryReportDownloadCtrl.js
@@ -7,25 +7,22 @@
   function InventoryReportDownloadCtrl($scope, UrlService) {
     var vm = this;
     vm.inventory = $scope.inventory;
-    console.log("Report sidebar Inventory ", vm.inventory);
-    vm.download = function() {
-      if(vm.downloadOptions.productEntries) {
+    vm.download = function () {
+      var buildLink = function (prefix) {
         var link = angular.element('<a/>');
         link.attr({
-          href: UrlService.url('/inventories/' + vm.inventory.id + '/product_entries.csv'),
+          href: UrlService.url('/inventories'/ + vm.inventory.id + '/' + prefix + '.csv'),
           target: '_blank',
-          download: 'inventory_' + vm.inventory.id + '_product_entries_report.csv'
+          download: 'inventory_' + vm.inventory.id + '_' + prefix + '_report.csv'
         })[0].click();
       }
-      if(vm.downloadOptions.dataEntries) {
-        var link = angular.element('<a/>');
-        link.attr({
-          href: UrlService.url('/inventories/' + vm.inventory.id + '/data_entries.csv'),
-          target: '_blank',
-          download: 'inventory_' + vm.inventory.id + '_data_entries_report.csv'
-        })[0].click();
+      if (vm.downloadOptions.productEntries) {
+        buildLink('product_entries');
+      }
+      if (vm.downloadOptions.dataEntries) {
+        buildLink('data_entries');
       }
       $scope.$emit('inventory-report-downloaded');
-    };
+    }
   }
 })();
