c47e25c998fcd0f81faf8bffafae062b3e15482d
Julio Santos <hi@whoisjuliosantos.com>
2016-04-25 10:51:33 +0100

    remove potencial yagni with analysis invitations

--- app/controllers/v1/analysis_invitations_controller.rb
+++ app/controllers/v1/analysis_invitations_controller.rb
@@ -6,7 +6,6 @@ class V1::AnalysisInvitationsController < ApplicationController
   def create
     create_params = invitation_params
     create_params[:analysis_id] = analysis.id
-    send_invite = create_params.delete(:send_invite)
 
     invite = AnalysisInvitation.new(create_params)
 
@@ -18,7 +17,6 @@ class V1::AnalysisInvitationsController < ApplicationController
 
     Analyses::MemberFromInvite.new(invite).execute
 
-    queue_worker(invite.id) if send_invite
     render nothing: true
   end
 
@@ -26,10 +24,6 @@ class V1::AnalysisInvitationsController < ApplicationController
   def invitation_params
     params
       .permit(:first_name, :last_name, :email, :team_role, :role)
-  end
-
-  def queue_worker(invite_id)
-    InventoryInvitationNotificationWorker.perform_async(invite_id)
   end
 
   def inventory_id
