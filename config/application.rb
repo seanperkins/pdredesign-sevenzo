195f4d7af8e7c1d2a10e0ea705cd42756071ecf3
Jason Black <makotothedragon@gmail.com>
2016-05-04 17:20:28 -0600

    Initial work on universal primary button

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,7 @@ module PdrServer
                                                Rails.root.join('app', 'assets', 'javascripts', 'client', 'permissions'),
                                                Rails.root.join('app', 'assets', 'javascripts', 'client', 'reminders'),
                                                Rails.root.join('app', 'assets', 'javascripts', 'client', 'home'),
+                                               Rails.root.join('app', 'assets', 'javascripts', 'client', 'buttons'),
                                                Rails.root.join('app', 'assets', 'javascripts', 'client', 'learning_questions')]
     config.angular_templates.markups        = %w(erb)
     config.angular_templates.htmlcompressor = false
