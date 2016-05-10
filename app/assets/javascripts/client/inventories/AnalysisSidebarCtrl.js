1285da980aeac44915a3a5757c94ea1888fbad73
bithavoc <im@bithavoc.io>
2016-05-10 14:33:36 -0500

    Added learning questions modal to analysis dashboard sidebar

--- app/assets/javascripts/client/inventories/AnalysisSidebarCtrl.js
+++ app/assets/javascripts/client/inventories/AnalysisSidebarCtrl.js
@@ -5,11 +5,25 @@
       .controller('AnalysisSidebarCtrl', AnalysisSidebarCtrl);
 
   AnalysisSidebarCtrl.$inject = [
-    'current_analysis'
+    'current_analysis',
+    '$scope',
+    '$modal'
   ];
 
-  function AnalysisSidebarCtrl(current_analysis) {
+  function AnalysisSidebarCtrl(current_analysis, $scope, $modal) {
     var vm = this;
+    $scope.analysis = current_analysis;
     vm.currentAnalysis = current_analysis;
+
+    vm.displayLearningQuestions = function() {
+      vm.questionsModal = $modal.open({
+        template: '<learning-question-modal context="analysis" reminder="false"></learning-question-modal>',
+        scope: $scope
+      });
+    };
+
+    $scope.$on('close-learning-question-modal', function() {
+      vm.questionsModal.dismiss('cancel');
+    });
   }
 })();
