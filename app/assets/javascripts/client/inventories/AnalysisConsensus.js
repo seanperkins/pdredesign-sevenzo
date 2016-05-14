37dc940f44f2cea48a1f5b678ec8e8133d85cef7
Jason Black <makotothedragon@gmail.com>
2016-05-13 18:33:33 -0600

    Refactor AnalysisConsensus

--- app/assets/javascripts/client/inventories/AnalysisConsensus.js
+++ app/assets/javascripts/client/inventories/AnalysisConsensus.js
@@ -1,10 +1,28 @@
-PDRClient.factory('AnalysisConsensus', ['$resource', 'UrlService', function($resource, UrlService) {
+(function() {
+  'use strict';
+
+  angular.module('PDRClient')
+      .factory('AnalysisConsensus', AnalysisConsensus);
+
+  AnalysisConsensus.$inject = [
+    '$resource',
+    'UrlService'
+  ];
+
+  function AnalysisConsensus($resource, UrlService) {
     return $resource(UrlService.url('inventories/:inventory_id/analyses/:analysis_id/analysis_consensus/:id'), null,
-      {
-      	'get'	: { method: 'GET', cache: true},
-        'create': { method: 'POST'},
-        'submit': { method: 'PUT'},
-        'pdf_report' : { url: UrlService.url('inventories/:inventory_id/analyses/:analysis_id/reports/consensus_report.pdf'), action: 'consensus_report' },
-        'report': { url: UrlService.url('inventories/:inventory_id/analyses/:analysis_id/analysis_consensus/:id/consensus_report'), action: 'consensus_report' }
-      });
-}]);
+        {
+          'get': {method: 'GET', cache: true},
+          'create': {method: 'POST'},
+          'submit': {method: 'PUT'},
+          'pdf_report': {
+            url: UrlService.url('inventories/:inventory_id/analyses/:analysis_id/reports/consensus_report.pdf'),
+            action: 'consensus_report'
+          },
+          'report': {
+            url: UrlService.url('inventories/:inventory_id/analyses/:analysis_id/analysis_consensus/:id/consensus_report'),
+            action: 'consensus_report'
+          }
+        });
+  }
+})();
