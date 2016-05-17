59e13e4e5b4d18798da0502d3dd85cc231dcafdf
Jason Black <makotothedragon@gmail.com>
2016-05-17 17:52:42 -0600

    Initial work on comparison query

---
+++ app/controllers/v1/analysis_reports_controller.rb
@@ -0,0 +1,34 @@
+class V1::AnalysisReportsController < ApplicationController
+  before_action :authenticate_user!
+
+  def comparison_data
+    @comparison_data = fetch_comparison_data
+  end
+
+  private
+  def fetch_comparison_data
+    GeneralInventoryQuestion.find_by_sql ["SELECT
+        giq.product_name,
+        giq.price_in_cents,
+        uq.usage
+      FROM general_inventory_questions giq
+        INNER JOIN product_entries pe ON giq.product_entry_id = pe.id
+        INNER JOIN usage_questions uq ON uq.product_entry_id = pe.id
+      WHERE pe.inventory_id IN (
+        SELECT DISTINCT product_entries.inventory_id
+        FROM product_entries
+          INNER JOIN general_inventory_questions ON general_inventory_questions.product_entry_id = product_entries.id
+        WHERE general_inventory_questions.id IN (
+          SELECT DISTINCT UNNEST(supporting_inventory_responses.product_entries)
+          FROM supporting_inventory_responses
+          WHERE supporting_inventory_responses.score_id IN (SELECT scores.id
+                                                            FROM scores
+                                                            WHERE scores.response_id = ?)))",
+                                         131]
+  end
+
+  def current_analysis
+    @analysis ||= Analysis.where(id: params[:analysis_id])
+  end
+
+end
