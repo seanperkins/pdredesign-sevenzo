b30c4eaa54af328940cc08eeccf318f3548c81c8
Júlio Santos <hi@whoisjuliosantos.com>
2016-05-26 11:01:24 +0100

    change share icon into check when copied

--- app/assets/javascripts/client/common_share/ShareUrlCtrl.js
+++ app/assets/javascripts/client/common_share/ShareUrlCtrl.js
@@ -10,11 +10,16 @@
   function ShareUrlCtrl($scope) {
     var vm = this;
 
+    vm.clipboard = new Clipboard('[data-clipboard-target]');
+
+    vm.clipboard.on('success', function () {
+      vm.copied = true;
+      $scope.$digest();
+    });
+
     $scope.$watch('url', function (url) {
       vm.url = url;
     });
-
-    new Clipboard('[data-clipboard-target]');
   }
 })();
 
