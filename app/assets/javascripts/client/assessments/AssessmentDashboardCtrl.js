2d5de0c5db04870d480b8d128f0d916c146f6563
Júlio Santos <hi@whoisjuliosantos.com>
2016-06-04 16:03:22 +0100

    assessment dashboard participant/message styles

--- app/assets/javascripts/client/assessments/AssessmentDashboardCtrl.js
+++ app/assets/javascripts/client/assessments/AssessmentDashboardCtrl.js
@@ -1,84 +1,69 @@
-PDRClient.controller('AssessmentDashboardCtrl', [
-  '$modal',
-  '$scope',
-  '$timeout',
-  '$stateParams',
-  'SessionService',
-  'Assessment',
-  'Participant',
-    function($modal, $scope, $timeout, $stateParams, SessionService, Assessment, Participant) {
-      $scope.id = $stateParams.id;
+(function () {
+  'use strict';
 
-      $scope.showModal = $stateParams.showModal || false;
-      $scope.currentUser = SessionService.getCurrentUser();
+  angular.module('PDRClient')
+    .controller('AssessmentDashboardCtrl', AssessmentDashboardCtrl);
 
-      $scope.$on('update_participants', function() {
-        $scope.updateParticipantsList();
-        $scope.updateAssessment();
-      });
+  AssessmentDashboardCtrl.$inject = [
+    '$modal',
+    '$scope',
+    '$timeout',
+    '$stateParams',
+    'SessionService',
+    'Assessment',
+    'Participant'
+  ];
 
-      $scope.updateParticipantsList = function() {
-        $scope.invitableParticipants = Participant.all({assessment_id: $scope.id});
-      };
+  function AssessmentDashboardCtrl ($modal, $scope, $timeout, $stateParams, SessionService, Assessment, Participant) {
+    $scope.id = $stateParams.id;
 
-      $scope.updateAssessment = function() {
-        Assessment
-          .get({id: $scope.id})
-          .$promise
-          .then(function(data){
-            $scope.assessment = data;
-            var averages = [];
-            var labels   = [];
+    $scope.showModal = $stateParams.showModal || false;
+    $scope.currentUser = SessionService.getCurrentUser();
 
-            var shortnames = {
-              "Teacher Engagement": "TE",
-              "Delivery Infrastructure": "DI",
-              "High Quality Content and Tools": "HQ",
-              "PD Process": "PP",
-              "Supportive Policies": "SP",
-              "Leadership Capacity": "LC",
-              "Data Infrastructure": "DTI",
-              "Resource Optimization": "RO"
-            };
-
-            angular.forEach(data.averages, function(value, key) {
-              labels.push(shortnames[key]);
-              averages.push(value);
-            });
-        });
-      };
-
-      $scope.addParticipant = function(user) {
-        Participant.save({assessment_id: $scope.id}, {user_id: user.id, send_invite: true});
-        $scope.updateParticipantsList();
-        $scope.updateAssessment();
-      };
-
-      $scope.updateAssessment();
+    $scope.$on('update_participants', function() {
       $scope.updateParticipantsList();
+      $scope.updateAssessment();
+    });
 
+    $scope.updateParticipantsList = function() {
+      $scope.invitableParticipants = Participant.all({assessment_id: $scope.id});
+    };
 
-      $scope.messageTitle = function(category) {
-        switch(category) {
-          case 'welcome':
-            return 'Welcome Invite';
-          case 'reminder':
-            return 'Individual Assessment Reminder';
-          default:
-            return 'General Message';
-        }
-      };
+    $scope.updateAssessment = function() {
+      Assessment
+        .get({id: $scope.id})
+        .$promise
+        .then(function(data){
+          $scope.assessment = data;
+          var averages = [];
+          var labels   = [];
 
-      $scope.messageIcon = function(category) {
-        switch(category) {
-          case 'welcome':
-            return 'fa-envelope-o';
-          case 'reminder':
-            return 'fa-clock-o';
-          default:
-            return 'fa-envelope-o';
-        }
-      };
+          var shortnames = {
+            "Teacher Engagement": "TE",
+            "Delivery Infrastructure": "DI",
+            "High Quality Content and Tools": "HQ",
+            "PD Process": "PP",
+            "Supportive Policies": "SP",
+            "Leadership Capacity": "LC",
+            "Data Infrastructure": "DTI",
+            "Resource Optimization": "RO"
+          };
 
-    }
-]);
+          angular.forEach(data.averages, function(value, key) {
+            labels.push(shortnames[key]);
+            averages.push(value);
+          });
+        });
+    };
+
+    $scope.addParticipant = function(user) {
+      Participant.save({assessment_id: $scope.id}, {user_id: user.id, send_invite: true});
+      $scope.updateParticipantsList();
+      $scope.updateAssessment();
+    };
+
+    $scope.updateAssessment();
+    $scope.updateParticipantsList();
+  }
+})();
+
