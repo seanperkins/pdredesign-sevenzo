a2a9473068381fc2b83045cb85c67c324be7747a
Júlio Santos <hi@whoisjuliosantos.com>
2016-06-04 14:59:41 +0100

    unify and style assessment dashboard/report; away with old assessment report sharing; away with assessment charts

--- app/assets/javascripts/client/controllers/sidebars/SidebarCtrl.js
+++ app/assets/javascripts/client/controllers/sidebars/SidebarCtrl.js
@@ -1,15 +1,25 @@
-PDRClient.controller('SidebarCtrl', ['$scope', '$modal', '$stateParams', 'SessionService',
-    function($scope, $modal, $stateParams, SessionService) {
-      $scope.user     = SessionService.getCurrentUser();
-      $scope.redirect = $stateParams.redirect;
+(function () {
+  'use strict';
 
-      if($scope.user) {
-        $scope.visible = true;
-        $scope.name   = $scope.user["full_name"];
-        $scope.avatar = $scope.user["avatar"];
-        $scope.role   = $scope.user["role_human"];
-      }
+  angular.module('PDRClient')
+      .controller('SidebarCtrl', SidebarCtrl);
 
+  SidebarCtrl.$inject = [
+    '$scope',
+    '$modal',
+    '$stateParams',
+    'SessionService'
+  ];
+
+  function SidebarCtrl($scope, $mmodal, $stateParams, SessionService) {
+    $scope.user     = SessionService.getCurrentUser();
+    $scope.redirect = $stateParams.redirect;
+
+    if($scope.user) {
+      $scope.visible = true;
+      $scope.name   = $scope.user["full_name"];
+      $scope.avatar = $scope.user["avatar"];
+      $scope.role   = $scope.user["role_human"];
     }
-
-]);
+  }
+})();
