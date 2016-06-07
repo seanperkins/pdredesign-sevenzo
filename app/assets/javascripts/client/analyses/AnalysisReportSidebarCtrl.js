4bc7afd66a26d2849c1ca0196cc86504f485b885
Júlio Santos <hi@whoisjuliosantos.com>
2016-06-07 15:01:41 +0100

    " view consensus" and "download csv" buttons on analysis report

--- app/assets/javascripts/client/analyses/AnalysisReportSidebarCtrl.js
+++ app/assets/javascripts/client/analyses/AnalysisReportSidebarCtrl.js
@@ -6,23 +6,32 @@
 
   AnalysisReportSidebarCtrl.$inject = [
     '$scope',
+    '$rootScope',
     '$state',
     '$stateParams',
+    'ConsensusService',
     'current_analysis'
   ];
 
-  function AnalysisReportSidebarCtrl($scope, $state, $stateParams, current_analysis) {
+  function AnalysisReportSidebarCtrl($scope, $rootScope, $state, $stateParams, ConsensusService, current_analysis) {
     var vm = this;
     vm.currentAnalysis = current_analysis;
     vm.shared = $stateParams.shared || false;
+    vm.consensusId = vm.currentAnalysis.consensus && vm.currentAnalysis.consensus.id;
     vm.shareURL = $state.href(
       'inventory_analysis_shared_report',
       {inventory_id: current_analysis.inventory_id, id: current_analysis.share_token},
       {absolute: true}
     );
-    vm.returnState = {
-      inventory_id: $stateParams.inventory_id,
-      id: $stateParams.id
+
+    ConsensusService.setContext('analysis');
+    vm.exportToCSV = function() {
+      $rootScope.$broadcast('building_export_file', {format: 'CSV'});
+      ConsensusService
+        .exportToCSV(vm.consensusId)
+        .then(function () {
+          $rootScope.$broadcast('stop_building_export_file');
+        });
     };
 
     $scope.$on('top-priority', function(evt, categoryObject) {
