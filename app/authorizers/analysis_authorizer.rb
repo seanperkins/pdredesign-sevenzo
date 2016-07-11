d158f34e68b96fc4f4093f17f8b0484e27ea1ff8
Jason Black <makotothedragon@gmail.com>
2016-07-11 14:28:47 -0600

    Allow inventory participants to facilitate new assessments

--- app/authorizers/analysis_authorizer.rb
+++ app/authorizers/analysis_authorizer.rb
@@ -1,23 +1,34 @@
 class AnalysisAuthorizer < ApplicationAuthorizer
   def readable_by?(user)
-    is_owner_or_facilitator_of_inventory?(user)
+    fetch_inventory_permissions(user)
   end
 
   def creatable_by?(user)
-    is_owner_or_facilitator_of_inventory?(user)
+    fetch_inventory_permissions(user)
   end
 
   def updatable_by?(user)
-    is_owner_or_facilitator_of_inventory?(user)
+    fetch_inventory_permissions(user)
   end
 
   def deletable_by?(user)
-    is_owner_or_facilitator_of_inventory?(user)
+    fetch_inventory_permissions(user)
   end
 
   private
-  def is_owner_or_facilitator_of_inventory?(user)
-    resource.inventory.try(:owner?, user: user) ||
-        resource.inventory.try(:facilitator?, user: user)
+  def fetch_inventory_permissions(user)
+    is_owner_of_inventory?(user) || is_facilitator_of_inventory?(user) || is_participant_of_inventory?(user)
+  end
+
+  def is_facilitator_of_inventory?(user)
+    resource.inventory.try(:facilitator?, user: user)
+  end
+
+  def is_participant_of_inventory?(user)
+    resource.inventory.try(:participant?, user: user)
+  end
+
+  def is_owner_of_inventory?(user)
+    resource.inventory.try(:owner?, user: user)
   end
 end
