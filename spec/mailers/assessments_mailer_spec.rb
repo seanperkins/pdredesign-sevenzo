41317d4b2eefc09f0f092a7461f9b10a7f2c2cd8
Jason Black <makotothedragon@gmail.com>
2016-07-28 11:24:09 -0600

    Refactor assessments_mailer_spec

--- spec/mailers/assessments_mailer_spec.rb
+++ spec/mailers/assessments_mailer_spec.rb
@@ -1,34 +1,43 @@
 require 'spec_helper'
 
 describe AssessmentsMailer do
-  before { create_magic_assessments }
-  let(:assessment) { @assessment_with_participants }
-  let(:subject)    { AssessmentsMailer }
-
-
   describe '#assigned' do
-    it 'sends the email to the correct email' do
-      mail = subject.assigned(assessment, @participant)
-      expect(mail.to).to include(@participant.user.email)
-    end
-    
-    it 'sends the corrent assessment link' do
-      mail = subject.assigned(assessment, @participant)
-      expect(mail.html_part.body).to include("/\#/assessments/#{assessment.id}/responses")
-      expect(mail.text_part.body).to include("/\#/assessments/#{assessment.id}/responses")
-    end
+    let(:assessment) {
+      create(:assessment, :with_participants)
+    }
+
+    let(:participant) {
+      assessment.participants.sample
+    }
+
+    it {
+      expect(AssessmentsMailer.assigned(assessment, participant).to include participant.user.email)
+    }
+
+    it {
+      expect(AssessmentsMailer.assigned(assessment, participant).html_part.body).to include("/\#/assessments/#{assessment.id}/responses")
+    }
+
+    it {
+      expect(AssessmentsMailer.assigned(assessment, participant).text_part.body).to include("/\#/assessments/#{assessment.id}/responses")
+    }
   end
 
   describe '#reminder' do
-    it 'sends the meail to the correct participant' do  
-      mail = subject.reminder(assessment, 'reminder', @participant)
-      expect(mail.to).to include(@participant.user.email)
-    end
+    let(:assessment) {
+      create(:assessment, :with_participants)
+    }
 
-    it 'sends the corrent assessment link' do
-      mail = subject.reminder(assessment, 'reminder', @participant)
-      expect(mail.body).to include("/\#/assessments/#{assessment.id}/responses")
-    end
+    let(:participant) {
+      assessment.participants.sample
+    }
 
+    it {
+      expect(AssessmentsMailer.reminder(assessment, 'reminder', participant).to).to include participant.user.email
+    }
+
+    it {
+      expect(AssessmentsMailer.reminder(assessment, 'reminder', participant).body).to include "/\#/assessments/#{assessment.id}/responses"
+    }
   end
 end
