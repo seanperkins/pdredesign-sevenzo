01e7c43b955a922ffbdb527efd11051915dd0da9
Jason Black <makotothedragon@gmail.com>
2016-09-13 09:13:36 -0600

    Finish off categoryContainerSpec

--- spec/javascripts/consensus/categoryContainerSpec.js
+++ spec/javascripts/consensus/categoryContainerSpec.js
@@ -76,9 +76,48 @@
         });
 
         describe('when container is clicked', function () {
-          it('toggles its visibility', function () {
+          it('adds the fa-chevron-down class', function () {
             element.find('.category-label').triggerHandler('click');
             expect(element.find('.category-label').find('i').hasClass('fa-chevron-down')).toBe(true);
+          });
+        });
+      });
+
+      describe('with the question type set to analysis', function () {
+        var template = '<category-container categories="categories" question-type="analysis" is-consensus="pass-thru" scores="scores"></category-container>';
+        var categories = [{
+          name: 'Foo'
+        }];
+
+        beforeEach(function () {
+          rootScope.categories = categories;
+          element = compile(template)(rootScope);
+          rootScope.$digest();
+        });
+
+        it('contains a question container entry', function () {
+          expect(element.find('question-container').length).toEqual(1);
+        });
+
+        it('does not display the category label', function () {
+          expect(element.find('.category-label').length).toEqual(0);
+        });
+
+        describe('when passing values to the question container directive', function () {
+          it('passes the category along', function () {
+            expect(element.find('question-container')[0].attributes['category'].value).toEqual('category')
+          });
+
+          it('passes the is-consensus value along', function () {
+            expect(element.find('question-container')[0].attributes['is-consensus'].value).toEqual('pass-thru');
+          });
+
+          it('passes the scores value along', function () {
+            expect(element.find('question-container')[0].attributes['scores'].value).toEqual('scores');
+          });
+
+          it('passes the question type value along', function () {
+            expect(element.find('question-container')[0].attributes['question-type'].value).toEqual('analysis');
           });
         });
       });
