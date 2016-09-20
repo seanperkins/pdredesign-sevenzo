e3c5ec9d1c030f0b76f72b5fbfc11e73a3c4d042
Jason Black <makotothedragon@gmail.com>
2016-09-20 09:43:21 -0600

    Finish up ToolMembers#create

--- spec/controllers/v1/tool_members_controller_spec.rb
+++ spec/controllers/v1/tool_members_controller_spec.rb
@@ -40,12 +40,73 @@ describe V1::ToolMembersController do
 
         before(:each) do
           sign_in user
-          post :create, tool_member: {tool_type: 'Assessment', tool_id: 1, role: 0, user_id: candidate.id}
+          post :create, tool_member: {tool_type: 'Assessment',
+                                      tool_id: tool.id,
+                                      role: ToolMember.member_roles[:participant],
+                                      user_id: candidate.id}
         end
 
         it {
           is_expected.to respond_with(:forbidden)
         }
+      end
+
+      context 'when the user is a facilitator on the tool' do
+        let(:user) {
+          create(:user)
+        }
+
+        let(:candidate) {
+          create(:user)
+        }
+
+        let(:tool) {
+          create(:assessment)
+        }
+
+        let!(:tool_member) {
+          create(:tool_member, :as_facilitator, tool: tool, user: user)
+        }
+
+        context 'when the entity to be created is valid' do
+          before(:each) do
+            sign_in user
+            post :create, tool_member: {tool_type: 'Assessment',
+                                        tool_id: tool.id,
+                                        role: ToolMember.member_roles[:participant],
+                                        user_id: candidate.id}
+          end
+
+          it {
+            is_expected.to respond_with(:created)
+          }
+
+          it {
+            expect(ToolMember.where(tool: tool, user: candidate).size).to eq 1
+          }
+        end
+
+        context 'when the entity to be created already exists' do
+          let!(:preexisting_candidate_membership) {
+            create(:tool_member, :as_participant, tool: tool, user: candidate)
+          }
+
+          before(:each) do
+            sign_in user
+            post :create, tool_member: {tool_type: 'Assessment',
+                                        tool_id: tool.id,
+                                        role: ToolMember.member_roles[:participant],
+                                        user_id: candidate.id}
+          end
+
+          it {
+            is_expected.to respond_with :bad_request
+          }
+
+          it {
+            expect(json['errors']['role'][0]).to eq 'has already been taken'
+          }
+        end
       end
     end
   end
