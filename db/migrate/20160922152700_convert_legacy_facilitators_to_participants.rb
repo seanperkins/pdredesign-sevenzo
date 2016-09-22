28684b972cd7bfa92fc564791bfd9f3d59d01a35
Jason Black <makotothedragon@gmail.com>
2016-09-22 14:13:57 -0600

    Revise legacy facilitator import script

--- db/migrate/20160922152700_convert_legacy_facilitators_to_participants.rb
+++ db/migrate/20160922152700_convert_legacy_facilitators_to_participants.rb
@@ -6,7 +6,11 @@ class ConvertLegacyFacilitatorsToParticipants < ActiveRecord::Migration
         .where.not(user_id: ToolMember.where(tool_type: 'Assessment',
                                              role: ToolMember.member_roles[:participant]).select(:user_id))
         .each { |tool_member|
-      ToolMember.create!(user: tool_member.user, tool: tool_member.tool, role: ToolMember.member_roles[:participant])
+      ToolMember.create!(user: tool_member.user,
+                         tool: tool_member.tool,
+                         role: ToolMember.member_roles[:participant],
+                         invited_at: tool_member.tool.created_at,
+                         reminded_at: nil)
     }
   end
 end
