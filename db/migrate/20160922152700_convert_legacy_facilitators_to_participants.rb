e491632c4adbfef9c78a8ef019a4d303af758914
Jason Black <makotothedragon@gmail.com>
2016-09-22 15:06:59 -0600

    Finish up data migration pieces for ToolMembers

--- db/migrate/20160922152700_convert_legacy_facilitators_to_participants.rb
+++ db/migrate/20160922152700_convert_legacy_facilitators_to_participants.rb
@@ -1,10 +1,6 @@
 class ConvertLegacyFacilitatorsToParticipants < ActiveRecord::Migration
   def change
-    ToolMember
-        .where(tool_type: 'Assessment',
-                     role: ToolMember.member_roles[:facilitator])
-        .where.not(user_id: ToolMember.where(tool_type: 'Assessment',
-                                             role: ToolMember.member_roles[:participant]).select(:user_id))
+    ToolMember.where(tool_type: 'Assessment', role: ToolMember.member_roles[:facilitator]).where.not(user_id: ToolMember.where(tool_type: 'Assessment', role: ToolMember.member_roles[:participant]).select(:user_id))
         .each { |tool_member|
       ToolMember.create!(user: tool_member.user,
                          tool: tool_member.tool,
